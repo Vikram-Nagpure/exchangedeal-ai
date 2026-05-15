1ce76571e42ce97a64d1209f7d143253f9926496
Vikram Nagpure <vikramnagpure99@gmail.com>
2026-05-15 15:43:27 +0530

    2ndhalf

---
+++ frontend/next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'm.media-amazon.com',
+      'rukminim2.flixcart.com',
+      'images.unsplash.com',
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${
+          process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'
+        }/api/:path*`,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
